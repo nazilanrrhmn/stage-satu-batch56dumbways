36e3cb4ee6435b59c04b9aac7639cf23a63c84d6
Mr. Nazil <nazilanurr99@gmail.com>
2024-08-10 11:58:19 +0700

    creating fitur login and register

--- app.js
+++ app.js
@@ -5,15 +5,36 @@ const expressLayouts = require('express-ejs-layouts');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
+const session = require('express-session');
+const bcrypt = require('bcrypt');
 const {
   Project
 } = require('./models'); // Import your model
+const {
+  QueryTypes
+} = require('sequelize');
+const sequelize = require('./models').sequelize; // Assuming your Sequelize instance is exported from models
 const app = express();
 const port = 3000;
+
+const saltRounds = 10; // Number of salt rounds for hashing
 
 // Middleware to parse form data
 app.use(express.urlencoded({
   extended: true
+}));
+
+// Session Middleware
+app.use(session({
+  secret: 'noname',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 360000,
+    secure: false,
+    httpOnly: true
+  },
+  store: new session.MemoryStore(),
 }));
 
 // Using EJS
@@ -39,11 +60,155 @@ const upload = multer({
   storage
 });
 
+// Ensure projects.json file exists
+function ensureProjectsFile() {
+  if (!fs.existsSync('projects.json')) {
+    fs.writeFileSync('projects.json', JSON.stringify([]));
+  }
+}
+
+// Login Function
+async function login(req, res) {
+  try {
+    console.log(req.body);
+
+    const query = `SELECT * FROM "Users" WHERE email = :email`;
+    const user = await sequelize.query(query, {
+      replacements: {
+        email: req.body.email
+      },
+      type: QueryTypes.SELECT
+    });
+
+    if (user.length === 0) {
+      console.log("Email belum terdaftar");
+      res.redirect('/login');
+      return;
+    }
+
+    // Compare the entered password with the hashed password
+    const match = await bcrypt.compare(req.body.password, user[0].password);
+    if (!match) {
+      console.log("Password salah");
+      res.redirect('/login');
+      return;
+    } else {
+      req.session.user = user[0];
+      req.session.isLogin = true;
+      console.log("Login Berhasil");
+      res.redirect('/');
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Server Error');
+  }
+}
+
+// Register Function
+async function register(req, res) {
+  try {
+    console.log(req.body);
+
+    const {
+      username,
+      email,
+      password
+    } = req.body;
+
+    // Validate input
+    if (!username || !email || !password) {
+      console.log("All fields are required");
+      return res.redirect('/register?error=All fields are required');
+    }
+
+    // Check if the email already exists
+    const query = `SELECT * FROM "Users" WHERE email = :email`;
+    const existingUser = await sequelize.query(query, {
+      replacements: {
+        email
+      },
+      type: QueryTypes.SELECT
+    });
+
+    if (existingUser.length > 0) {
+      console.log("Email sudah terdaftar");
+      return res.redirect('/register?error=Email sudah terdaftar');
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    // Insert new user
+    const insertQuery = `INSERT INTO "Users" 
+                          (username, email, password, "createdAt", "updatedAt")
+                          VALUES 
+                          (:username, :email, :password, NOW(), NOW())`;
+
+    await sequelize.query(insertQuery, {
+      replacements: {
+        username,
+        email,
+        password: hashedPassword
+      }
+    });
+
+    res.redirect('/login?success=Registration successful');
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Server Error');
+  }
+}
+
+// Middleware to attach session data to res.locals
+app.use((req, res, next) => {
+  res.locals.isLogin = req.session.isLogin;
+  res.locals.user = req.session.user;
+  next();
+});
+
 // Home Page
 app.get('/', (req, res) => {
   res.render('index', {
     layout: 'layouts/main-layout',
     title: "B56 - Home Page"
+  });
+});
+
+// Login Page
+app.get('/login', (req, res) => {
+  res.render('login', {
+    layout: 'layouts/main-layout',
+    title: "B56 - Login"
+  });
+});
+
+// Handle Login Form Submission
+app.post('/login', login);
+
+// Registration Page
+app.get('/register', (req, res) => {
+  const error = req.query.error;
+  const success = req.query.success;
+
+  res.render('register', {
+    layout: 'layouts/main-layout',
+    title: "B56 - Register",
+    error,
+    success
+  });
+});
+
+// Handle Registration Form Submission
+app.post('/register', register);
+
+// LOGOUT
+app.get('/logout', (req, res) => {
+  req.session.destroy(err => {
+    if (err) {
+      console.error('Error destroying session:', err);
+      return res.status(500).send('Server Error');
+    }
+    res.redirect('/login');
   });
 });
 
@@ -79,8 +244,6 @@ app.post('/add-project', upload.single('image'), async (req, res) => {
     description,
     technologies
   } = req.body;
-
-  // Ensure technologies is an array
   const techArray = Array.isArray(technologies) ? technologies : [technologies];
 
   try {
@@ -89,9 +252,9 @@ app.post('/add-project', upload.single('image'), async (req, res) => {
       startDate,
       endDate,
       description,
-      technologies: techArray, // Store technologies as an array
+      technologies: techArray,
       image: req.file ? `/uploads/${req.file.filename}` : "",
-      postAt: new Date() // Set postAt to the current date
+      postAt: new Date()
     });
     res.redirect('/project');
   } catch (err) {
@@ -122,9 +285,8 @@ app.get('/edit-project/:projectId', async (req, res) => {
   }
 });
 
-
 app.post('/edit-project/:projectId', upload.single('image'), async (req, res) => {
-  const projectId = parseInt(req.params.projectId, 10); // Ambil projectId dari parameter URL
+  const projectId = parseInt(req.params.projectId, 10);
   const {
     projectName,
     startDate,
@@ -134,25 +296,22 @@ app.post('/edit-project/:projectId', upload.single('image'), async (req, res) =>
   } = req.body;
 
   try {
-    // Fetch the current project to get the existing `postAt`
     const project = await Project.findByPk(projectId);
     if (!project) {
       return res.status(404).send('<h1>404 - Project Not Found</h1>');
     }
 
-    // Convert technologies to a string if it is an array
     const technologiesArray = Array.isArray(technologies) ? technologies : [technologies];
     const technologiesString = technologiesArray.join(',');
 
-    // Update project details
     await Project.update({
       projectName,
       startDate,
       endDate,
       description,
       technologies: technologiesString,
-      image: req.file ? `/uploads/${req.file.filename}` : project.image, // Preserve existing image if not updated
-      postAt: project.postAt // Keep original postAt value
+      image: req.file ? `/uploads/${req.file.filename}` : project.image,
+      postAt: project.postAt
     }, {
       where: {
         id: projectId
@@ -190,25 +349,21 @@ app.post('/delete-project/:projectId', async (req, res) => {
 
 // Project Detail Page
 app.get('/detail-project/:projectId', async (req, res) => {
-  const projectId = parseInt(req.params.projectId, 10); // Ambil projectId dari parameter URL
+  const projectId = parseInt(req.params.projectId, 10);
 
   try {
-    // Cek apakah projectId valid
     if (isNaN(projectId)) {
       return res.status(400).send('<h1>400 - Bad Request</h1>');
     }
 
-    // Fetch project details
     const project = await Project.findByPk(projectId);
     if (project) {
-      // Render halaman detail proyek
       res.render('detail-project', {
         layout: 'layouts/main-layout',
         title: "B56 - Project Detail",
         project
       });
     } else {
-      // Jika proyek tidak ditemukan
       res.status(404).send('<h1>404 - Project Not Found</h1>');
     }
   } catch (err) {
